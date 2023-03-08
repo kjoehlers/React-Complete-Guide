d962f669e42b73e852b380dc86e7e98b488249d2
Ken Oehlers <kjojr56@gmail.com>
2023-03-07 19:30:55 -0500

    Commit after creating testdata file

--- src/App.js
+++ src/App.js
@@ -4,27 +4,40 @@ import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+const testData = require("./data/testdata.json");
+// console.log(testData);
+
+const DUMMY_EXPENSES = testData.map((item) => {
+  return {
+    id: item.id,
+    title: item.description,
+    amount: item.price,
+    date: new Date(item.date),
+  };
+});
+// console.log(DUMMY_EXPENSES);
+
+// const DUMMY_EXPENSES = [
+//   {
+//     id: "e1",
+//     title: "Toilet Paper",
+//     amount: 94.12,
+//     date: new Date(2020, 7, 14),
+//   },
+//   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+//   {
+//     id: "e3",
+//     title: "Car Insurance",
+//     amount: 294.67,
+//     date: new Date(2021, 2, 28),
+//   },
+//   {
+//     id: "e4",
+//     title: "New Desk (Wooden)",
+//     amount: 450,
+//     date: new Date(2021, 5, 12),
+//   },
+// ];
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
